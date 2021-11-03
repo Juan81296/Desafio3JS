6fca26da7c62cca2751062326703d2abfb7211ce
Juan Tenaglia <juantenaglia81296@gmail.com>
2021-11-03 10:27:06 -0300

    Desafio3

---
+++ desafio3.js
@@ -0,0 +1,8 @@
+let usuario = prompt("Ingrese su nombre de usuario o ESC para salir");
+
+while ( usuario != "ESC"){
+
+    console.log("Bienvenido: " + usuario);
+
+    usuario = prompt("Ingrese su nombre de usuario o ESC para salir")
+}
